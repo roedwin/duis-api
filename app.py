74d2349a9e79f0475db70eea8c1769f954f439ef
Edwin Rivera <roedwin92@gmail.com>
2023-05-07 10:51:59 -0600

    ApiREST con FastAPI completada con CRUD en un 100%

--- app.py
+++ app.py
@@ -45,3 +45,13 @@ def delete_post(post_id: str):
             posts.pop(index)
             return {"message": "Post eliminado"}
     raise HTTPException(status_code=404, detail="Post Not found")
+
+@app.put("/posts/{post_id}")
+def update_post(post_id: str, updatedPost: Post):
+    for index, post in enumerate(posts):
+        if post["id"] == post_id:
+          posts[index]["title"] = updatedPost.title
+          posts[index]["content"] = updatedPost.content
+          posts[index]["author"] = updatedPost.author
+          return {"message": "Post actualizado"}
+    raise HTTPException(status_code=404, detail="Post Not found")
